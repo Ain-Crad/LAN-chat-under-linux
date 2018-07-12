fcb17b65be55ea6e261e5962407c423fd04dac31
Aincrad <851056505@qq.com>
2018-07-12 09:07:57 +0800

    add sign up with email

--- tcp_client.c
+++ tcp_client.c
@@ -13,6 +13,7 @@
 #include <arpa/inet.h>
 #include <pthread.h>
 #include <stdbool.h>
+#include <time.h>
 
 #define portnumber 8633
 #define MAX_BUF_SIZE 100
@@ -22,7 +23,24 @@ void *rec_data(void *fd);
 struct infstr{
 	char user_str[100];
 	char ip_str[100];
+	char passwd_str[100];
 } inf;
+
+struct sign_up{
+	char user_sign[100];
+	char passwd_sign[100];
+	char email_sign[100];
+} sign;
+
+bool exist;
+bool signfail;
+
+const char sign_failed[] = "The user has been signed\0";
+const char verif_failed[] = "The verification code is wrong\0";
+const char log_failed[] = "The user name or the password is not correct\0";
+char log_succ[] = "login successful, enjoy the chat\n*********************************************************";
+char sign_succ[] = "sign up successful, you can log in now or continue to sign up a new user.\n*********************************************************************\n";
+const char conti[] = "continue\0";
 
 int main(int argc, char *argv[]){
 	const char *eth_name = "wlp6s0";
@@ -31,6 +49,9 @@ int main(int argc, char *argv[]){
 	char model[100];
 	char buffer[1024];
 	char user[100];
+	char passwd[100];
+	char email[100];
+	char verif_code[100];
 	char send_msg[100];
 	struct sockaddr_in server_addr;
 	struct hostent *host;
@@ -66,41 +87,104 @@ int main(int argc, char *argv[]){
 	}
 	get_local_ip(eth_name, ip, sockfd);
 	
-	printf("log in or sign up\n");
-	fgets(model, MAX_BUF_SIZE, stdin);
+	
 	while(1){
-		if(strcmp(model,"log in\n") == 0){
+		signfail = false;
+		printf("log in or sign up\n");
+		fgets(model, MAX_BUF_SIZE, stdin);
+		model[strlen(model) - 1] = '\0';
+		if(write(sockfd, model, sizeof(model)) == -1){
+			fprintf(stderr, "Write Error:%s\n", strerror(errno));
+			exit(1);
+		}
+		
+		if(strcmp(model,"log in") == 0){
 			printf("user:");
 			fgets(user, MAX_BUF_SIZE, stdin);
-			for(i = 0; i < MAX_BUF_SIZE; i++){
-				if(user[i] == '\n'){
-					user[i] = '\0';
-					break;
-				}
-			}
+			user[strlen(user) - 1] = '\0';
+			
+			printf("passwd:");
+			fgets(passwd, MAX_BUF_SIZE, stdin);
+			passwd[strlen(passwd) - 1] = '\0';
+			
+			
 			strcpy(inf.user_str, user);
 			strcpy(inf.ip_str, ip);
-			if(write(sockfd, &inf, sizeof(inf)) == -1){
+			strcpy(inf.passwd_str, passwd);
+			//printf("writing\n");
+			if(write(sockfd, &inf, sizeof(struct infstr)) == -1){
 				fprintf(stderr, "Write Error:%s\n", strerror(errno));
 				exit(1);
 			}
-			
+			//printf("write success\n");
 			if((nbytes = read(sockfd, buffer, 1024)) == -1){
 				fprintf(stderr, "Read Error:%s\n", strerror(errno));
 				exit(1);
 			}
 			buffer[nbytes] = '\0';
 			printf("%s\n", buffer);
-			break;
+			if(strcmp(buffer, log_succ) == 0){
+				break;
+			}
 		}
-		else if(strcmp(model,"sign up\n") == 0){
+		else if(strcmp(model,"sign up") == 0){
+			printf("user:");
+			fgets(user, MAX_BUF_SIZE, stdin);
+			user[strlen(user) - 1] = '\0';
+			strcpy(sign.user_sign, user);
+			printf("passwd:");
+			fgets(passwd, MAX_BUF_SIZE, stdin);
+			passwd[strlen(passwd) - 1] = '\0';
+			strcpy(sign.passwd_sign, passwd);
+			printf("email:");
+			fgets(email, MAX_BUF_SIZE, stdin);
+			email[strlen(email) - 1] = '\0';
+			strcpy(sign.email_sign, email);
+			if(write(sockfd, &sign, sizeof(sign)) == -1){
+				fprintf(stderr, "(sign)Write Error:%s\n", strerror(errno));
+				exit(1);
+			}
 			
-			break;
+			if((nbytes = read(sockfd, buffer, sizeof(buffer))) == -1){
+				fprintf(stderr, "Read Error:%s\n", strerror(errno));
+				exit(1);
+			}
+			buffer[nbytes] = '\0';
+			
+			if(strcmp(buffer, sign_failed) == 0){
+				printf("%s\n", sign_failed);
+				continue;
+			}
+			else{
+				printf("verif_code:");
+				fgets(verif_code, MAX_BUF_SIZE, stdin);
+				verif_code[strlen(verif_code) - 1] = '\0';
+				if(write(sockfd, verif_code, sizeof(verif_code)) == -1){
+					fprintf(stderr, "(sign)Write Error:%s\n", strerror(errno));
+					exit(1);
+				}
+				if((nbytes = read(sockfd, buffer, sizeof(buffer))) == -1){
+					fprintf(stderr, "Read Error:%s\n", strerror(errno));
+					exit(1);
+				}
+				buffer[nbytes] = '\0';
+				printf("%s\n", buffer);
+				if(strcmp(buffer, sign_succ) == 0){
+					//printf("%s\n", sign_succ);
+					continue;
+				}
+				else if(strcmp(buffer, verif_failed) == 0){
+					//printf("%s\n", verif_failed);
+					continue;
+				}
+				
+			}
 		}
+		
 		else{
-			printf("No such a choice, please input again:");
+			printf("No such a choice, please input again\n");
 			memset(model, 0, sizeof(model));
-			fgets(model, MAX_BUF_SIZE, stdin);
+			//fgets(model, MAX_BUF_SIZE, stdin);
 		}
 	}
 	
@@ -112,6 +196,7 @@ int main(int argc, char *argv[]){
 	
 	while(1){
 		memset(send_msg, 0, sizeof(send_msg));
+		exist = true;
 		//printf("send:");
 		fgets(send_msg, MAX_BUF_SIZE, stdin);
 		for(i = 0; i < strlen(send_msg); i++){
@@ -128,30 +213,39 @@ int main(int argc, char *argv[]){
 			break;
 		}
 		else if(strcmp(send_msg, "(sendTo)")==0){
-			fgets(send_msg, MAX_BUF_SIZE, stdin);
-			for(i = 0; i < strlen(send_msg); i++){
-				if(send_msg[i] == '\n'){
-					send_msg[i] = '\0';
+			while(1){
+				fgets(send_msg, MAX_BUF_SIZE, stdin);
+				for(i = 0; i < strlen(send_msg); i++){
+					if(send_msg[i] == '\n'){
+						send_msg[i] = '\0';
+						break;
+					}
+				}
+				if(write(sockfd, send_msg, sizeof(send_msg)) == -1){
+					fprintf(stderr, "Write Error:%s\n", strerror(errno));
+					exit(1);
+				}
+				if(exist){
+					fgets(send_msg, MAX_BUF_SIZE, stdin);
+					for(i = 0; i < strlen(send_msg); i++){
+						if(send_msg[i] == '\n'){
+							send_msg[i] = '\0';
+							break;
+						}
+					}
+					if(write(sockfd, send_msg, sizeof(send_msg)) == -1){
+						fprintf(stderr, "Write Error:%s\n", strerror(errno));
+						exit(1);
+					}
+					printf("(private message)\n");
 					break;
 				}
-			}
-			if(write(sockfd, send_msg, sizeof(send_msg)) == -1){
-				fprintf(stderr, "Write Error:%s\n", strerror(errno));
-				exit(1);
+				else{
+					continue;
+				}
+			
 			}
 			
-			fgets(send_msg, MAX_BUF_SIZE, stdin);
-			for(i = 0; i < strlen(send_msg); i++){
-				if(send_msg[i] == '\n'){
-					send_msg[i] = '\0';
-					break;
-				}
-			}
-			if(write(sockfd, send_msg, sizeof(send_msg)) == -1){
-				fprintf(stderr, "Write Error:%s\n", strerror(errno));
-				exit(1);
-			}
-			printf("(private message)\n");
 		}
 	}
 
@@ -194,7 +288,14 @@ void *rec_data(void *fd){
 			exit(1);
 		}
 		rec_msg[nbytes] = '\0';
-		printf("%s\n", rec_msg);
+		if(rec_msg[nbytes - 1] == '*'){
+			exist = false;
+			printf("%s\n", rec_msg);
+		}
+		else{
+			printf("\t\t\t%s\n", rec_msg);
+		}
+		
 	}
 	//free(fd);
 	//close(sock_fd);
--- tcp_server.c
+++ tcp_server.c
@@ -13,19 +13,32 @@
 #include <arpa/inet.h>
 #include <pthread.h>
 #include <stdbool.h>
+#include <time.h> 
 
 #define portnumber 8633
 
 void *rec_data(void *fd);
+void email(char *email_send, char *verif);
+void sendemail(char *email, char *body);
+int open_socket(struct sockaddr *addr);
 
 struct infnew{
 	char user_new[100];
 	char ip_new[100];
+	char passwd_new[100];
 } inf;
+
+struct sign_up{
+	char user_sign[100];
+	char passwd_sign[100];
+	char email_sign[100];
+} sign;
 
 struct data_usr{
 	char data_user[100];
 	char data_ip[100];
+	char data_passwd[100];
+	char data_email[100];
 	pthread_t data_thread;
 	int data_fd;
 	bool online;
@@ -35,6 +48,30 @@ int cnt;
 bool user_old;
 const char ch[3] = ":\0";
 const char priv[20] = "(private message)\0";
+const char no_user[30] = "*no such a user, try again*\0";
+const char l[3] = "(\0";
+const char r[3] = ")\0";
+const char sign_failed[] = "The user has been signed\0";
+const char verif_failed[] = "The verification code is wrong\0";
+const char log_failed[] = "The user name or the password is not correct\0";
+const char conti[] = "continue\0";
+char buffer[100];
+
+char verif_code[10];
+/******email-setting******/
+struct data6
+{
+	unsigned int d4:6;
+	unsigned int d3:6;
+	unsigned int d2:6;
+	unsigned int d1:6;
+};
+// 协议中加密部分使用的是base64方法
+char con628(char c6);
+void base64(char *dbuf,char *buf128,int len);
+void sendemail(char *email,char *body);
+int open_socket(struct sockaddr *addr);
+/******email-setting******/
 
 int main(int argc, char *argv[]){
 	int sockfd, new_fd;
@@ -43,7 +80,8 @@ int main(int argc, char *argv[]){
 	int sin_size;
 	int nbytes;
 	char hello[] = "Long time no see, how are you?\n";
-	char succ[] = "login successful, enjoy the chat\n";
+	char log_succ[] = "login successful, enjoy the chat\n*********************************************************";
+char sign_succ[] = "sign up successful, you can log in now or continue to sign up a new user.\n*********************************************************************\n";
 	int i = 0;
 	
 	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
@@ -70,6 +108,7 @@ int main(int argc, char *argv[]){
 	cnt = 0;
 	while(1){
 		memset(&inf, 0, sizeof(inf));//初始化
+		memset(buffer, 0, sizeof(buffer));
 		user_old = false;
 		pthread_t thread;//创建新的线程接收客户端数据
 		sin_size = sizeof(struct sockaddr_in);
@@ -79,42 +118,110 @@ int main(int argc, char *argv[]){
 		}
 		//fprintf(stderr, "Server get connection from %s\n\a", inet_ntoa(client_addr.sin_addr));
 		printf("Server get connection from %s\n\a", inet_ntoa(client_addr.sin_addr));
-		if((nbytes = read(new_fd, &inf, sizeof(inf))) == -1){
-			fprintf(stderr, "Read Error:%s\n", strerror(errno));
-			exit(1);
-		}
-		//return login successful
-		if(write(new_fd, succ, strlen(succ)) == -1){
-			fprintf(stderr, "Write Error:%s\n", strerror(errno));
-			exit(1);
-		}
-		//judge if it's the old user
-		for(i = 0; i < cnt; i++){
-			if(inf.user_new == data[i].data_user){
-				user_old = true;
-				strcpy(data[cnt].data_ip, inf.ip_new);
-				data[i].data_thread = thread;
-				data[i].data_fd = new_fd;
-				data[i].online = true;
-				break;
+		
+		
+		while(1){
+			int tmp;
+			if((nbytes = read(new_fd, buffer, sizeof(buffer))) == -1){
+				fprintf(stderr, "Read Error:%s\n", strerror(errno));
+				exit(1);
+			}
+			if(strcmp(buffer, "log in") == 0){
+				if((nbytes = read(new_fd, &inf, sizeof(struct infnew))) == -1){
+					fprintf(stderr, "Read Error:%s\n", strerror(errno));
+					exit(1);
+				}
+				for(i = 0; i < cnt; i++){
+					if(strcmp(data[i].data_user, inf.user_new) == 0){
+						if(strcmp(data[i].data_passwd, inf.passwd_new) == 0){
+							strcpy(data[i].data_ip, inf.ip_new);
+							data[i].data_thread = thread;
+							data[i].online = true;
+							break;
+						}
+					}
+				}
+				//printf("I am here\n");
+				if(i >= cnt){
+					//printf("I am in if\n");
+					if(write(new_fd, log_failed, strlen(log_failed)) == -1){
+						fprintf(stderr, "Write Error:%s\n", strerror(errno));
+						exit(1);
+					}
+				}
+				else{
+					//printf("I am in else\n");
+					if(write(new_fd, log_succ, strlen(log_succ)) == -1){
+						fprintf(stderr, "Write Error:%s\n", strerror(errno));
+						exit(1);
+					}
+					break;
+				}
+				
+			}
+			else if(strcmp(buffer, "sign up") == 0){
+				if((nbytes = read(new_fd, &sign, sizeof(struct sign_up))) == -1){
+					fprintf(stderr, "Read Error:%s\n", strerror(errno));
+					exit(1);
+				}
+				
+				for(i = 0; i < cnt; i++){
+					if(sign.user_sign == data[i].data_user){
+						user_old = true;
+						break;
+					}
+				}
+				
+				if(user_old){
+					if(write(new_fd, sign_failed, strlen(sign_failed)) == -1){
+						fprintf(stderr, "Write Error:%s\n", strerror(errno));
+						exit(1);
+					}
+					user_old = false;
+					continue;
+				}
+				else{
+					if(write(new_fd, conti, strlen(conti)) == -1){
+						fprintf(stderr, "Write Error:%s\n", strerror(errno));
+						exit(1);
+					}
+				}
+				srand((unsigned)(time(NULL)));
+				tmp = rand() % 10000;
+				sprintf(verif_code, "%04d", tmp);
+				printf("verif_code:%s\n", verif_code);
+				email(sign.email_sign, verif_code);
+				if((nbytes = read(new_fd, buffer, sizeof(buffer))) == -1){
+					fprintf(stderr, "Read Error:%s\n", strerror(errno));
+					exit(1);
+				}
+				//注册成功
+				if(strcmp(buffer, verif_code) == 0){
+					if(write(new_fd, sign_succ, strlen(sign_succ)) == -1){
+						fprintf(stderr, "Write Error:%s\n", strerror(errno));
+						exit(1);
+					}
+					strcpy(data[cnt].data_user, sign.user_sign);
+					strcpy(data[cnt].data_passwd, sign.passwd_sign);
+					strcpy(data[cnt].data_email, sign.email_sign);
+					data[cnt].data_fd = new_fd;
+					cnt++;
+				}
+				else{
+					printf("buffer:%s\n", buffer);
+					if(write(new_fd, verif_failed, strlen(verif_failed)) == -1){
+						fprintf(stderr, "Write Error:%s\n", strerror(errno));
+						exit(1);
+					}
+				}
 			}
 		}
-		if(!user_old){
-			strcpy(data[cnt].data_user, inf.user_new);
-			strcpy(data[cnt].data_ip, inf.ip_new);
-			data[cnt].data_thread = thread;
-			data[cnt].data_fd = new_fd;
-			data[cnt].online = true;
-			cnt++;
-		}
+		
 		//create new thread
 		if(pthread_create(&thread, NULL, (void *)rec_data, &new_fd) != 0){
 			fprintf(stderr, "Create thread Error:%s\n", strerror(errno));
 			exit(1);
 		}
-		//printf("user:%s\n", inf.user_str);
-		//printf("ip:%s\n", inf.ip_str);
-		//close(new_fd);
 	}
 	
 	close(sockfd);
@@ -134,12 +241,15 @@ void *rec_data(void *fd){
 	
 	while(1){
 		memset(msg, 0, sizeof(msg));
+		memset(user_name, 0, sizeof(user_name));
+		strcpy(user_name, l);
 		for(k = 0; k < cnt; k++){
 			if(data[k].online == true && data[k].data_fd == client_fd){
-				strcpy(user_name, data[k].data_user);
+				strcat(user_name, data[k].data_user);
 				break;
 			}
 		}
+		strcat(user_name, r);
 		if((nbytes = read(client_fd, msg, sizeof(msg))) == -1){
 			fprintf(stderr, "Read Error:%s\n", strerror(errno));
 			exit(1);
@@ -162,38 +272,55 @@ void *rec_data(void *fd){
 		}
 		else if(strcmp(msg, "(sendTo)")==0){
 			int i;
-			if((nbytes = read(client_fd, object, sizeof(object))) == -1){
-				fprintf(stderr, "Read Error:%s\n", strerror(errno));
-				exit(1);
-			}
-			object[nbytes] = '\0';
-			if((nbytes = read(client_fd, msg, sizeof(msg))) == -1){
-				fprintf(stderr, "Read Error:%s\n", strerror(errno));
-				exit(1);
-			}
-			msg[nbytes] = '\0';
-			
-			for(k = 0; k < cnt; k++){
-				if(data[k].online == true && data[k].data_fd == client_fd){
-					strcpy(user_name, data[k].data_user);
-					break;
+			while(1){
+				if((nbytes = read(client_fd, object, sizeof(object))) == -1){
+					fprintf(stderr, "Read Error:%s\n", strerror(errno));
+					exit(1);
 				}
-			}
-			strcat(user_name, ch);
-			strcat(user_name, msg);
-			strcat(user_name, priv);
-			for(i = 0; i < cnt; i++){
-				if(data[i].online == true && strcmp(data[i].data_user, object) == 0){
-					fd_tmp = data[i].data_fd;
-					break;
+				object[nbytes] = '\0';
+				for(i = 0; i < cnt; i++){
+					if(data[i].online == true && strcmp(data[i].data_user, object) == 0){
+						break;
+					}
 				}
-			}
-			for(i = 0; i < cnt; i++){
-				if(data[i].online == true && data[i].data_fd == fd_tmp){
-					if(write(data[i].data_fd, user_name, strlen(user_name)) == -1){
+				if(i >= cnt){
+					if(write(client_fd, no_user, strlen(no_user)) == -1){
 						fprintf(stderr, "Write Error:%s\n", strerror(errno));
 						exit(1);
 					}
+				}
+				else{
+					if((nbytes = read(client_fd, msg, sizeof(msg))) == -1){
+						fprintf(stderr, "Read Error:%s\n", strerror(errno));
+						exit(1);
+					}
+					msg[nbytes] = '\0';
+					strcpy(user_name, l);
+					for(k = 0; k < cnt; k++){
+						if(data[k].online == true && data[k].data_fd == client_fd){
+							strcat(user_name, data[k].data_user);
+							break;
+						}
+					}
+					strcat(user_name, r);
+					strcat(user_name, ch);
+					strcat(user_name, msg);
+					strcat(user_name, priv);
+					for(i = 0; i < cnt; i++){
+						if(data[i].online == true && strcmp(data[i].data_user, object) == 0){
+							fd_tmp = data[i].data_fd;
+							break;
+						}
+					}
+					for(i = 0; i < cnt; i++){
+						if(data[i].online == true && data[i].data_fd == fd_tmp){
+							if(write(data[i].data_fd, user_name, strlen(user_name)) == -1){
+								fprintf(stderr, "Write Error:%s\n", strerror(errno));
+								exit(1);
+							}
+						}
+					}
+					break;
 				}
 			}
 		}
@@ -206,6 +333,9 @@ void *rec_data(void *fd){
 					list[strlen(list)] = '\t';
 					strcat(list, data[i].data_ip);
 					list[strlen(list)] = '\n';
+					list[strlen(list)] = '\t';
+					list[strlen(list)] = '\t';
+					list[strlen(list)] = '\t';
 				}
 			}
 			list[strlen(list)] = '\0';
@@ -232,7 +362,211 @@ void *rec_data(void *fd){
 	pthread_exit(NULL);
 }
 
+/********sendEmail********/
 
+void email(char *email_send, char *verif){
+	char email_rec[100];
+	strcpy(email_rec, email_send);
+	char body[] = "From: \"Aincrad\"<851056505@qq.com>\r\n"
+	"To: \"dasiy\"<851056505@qq.com>\r\n"
+	"Subject: verification code\r\n\r\n";
+	strcat(body, verif);
+	//"Hello World, Hello Email!";
+	sendemail(email_rec, body);
+}
 
-
+char con628(char c6)
+{
+	char rtn = '\0';
+	if (c6 < 26) rtn = c6 + 65;
+	else if (c6 < 52) rtn = c6 + 71;
+	else if (c6 < 62) rtn = c6 - 4;
+	else if (c6 == 62) rtn = 43;
+	else rtn = 47;
+	return rtn;
+}
+ 
+// base64的实现
+void base64(char *dbuf, char *buf128, int len)
+{
+	struct data6 *ddd = NULL;
+	int i = 0;
+	char buf[256] = {0};
+	char *tmp = NULL;
+	char cc = '\0';
+	memset(buf, 0, 256);
+	strcpy(buf, buf128);
+	for(i = 1; i <= len/3; i++)
+	{
+	tmp = buf+(i-1)*3;
+	cc = tmp[2];
+	tmp[2] = tmp[0];
+	tmp[0] = cc;
+	ddd = (struct data6 *)tmp;
+	dbuf[(i-1)*4+0] = con628((unsigned int)ddd->d1);
+	dbuf[(i-1)*4+1] = con628((unsigned int)ddd->d2);
+	dbuf[(i-1)*4+2] = con628((unsigned int)ddd->d3);
+	dbuf[(i-1)*4+3] = con628((unsigned int)ddd->d4);
+	}
+	if(len%3 == 1)
+	{
+	tmp = buf+(i-1)*3;
+	cc = tmp[2];
+	tmp[2] = tmp[0];
+	tmp[0] = cc;
+	ddd = (struct data6 *)tmp;
+	dbuf[(i-1)*4+0] = con628((unsigned int)ddd->d1);
+	dbuf[(i-1)*4+1] = con628((unsigned int)ddd->d2);
+	dbuf[(i-1)*4+2] = '=';
+	dbuf[(i-1)*4+3] = '=';
+	}
+	if(len%3 == 2)
+	{
+	tmp = buf+(i-1)*3;
+	cc = tmp[2];
+	tmp[2] = tmp[0];
+	tmp[0] = cc;
+	ddd = (struct data6 *)tmp;
+	dbuf[(i-1)*4+0] = con628((unsigned int)ddd->d1);
+	dbuf[(i-1)*4+1] = con628((unsigned int)ddd->d2);
+	dbuf[(i-1)*4+2] = con628((unsigned int)ddd->d3);
+	dbuf[(i-1)*4+3] = '=';
+	}
+	return;
+}
+// 发送邮件
+void sendemail(char *email, char *body)
+{
+	int sockfd = 0;
+	struct sockaddr_in their_addr = {0};
+	char buf[1500] = {0};
+	char rbuf[1500] = {0};
+	char login[128] = {0};
+	char pass[128] = {0};
+	#ifdef WIN32
+	WSADATA WSAData;
+	WSAStartup(MAKEWORD(2, 2), &WSAData);
+	#endif
+	memset(&their_addr, 0, sizeof(their_addr));
+	their_addr.sin_family = AF_INET;
+	their_addr.sin_port = htons(25);//25
+	their_addr.sin_addr.s_addr = inet_addr("14.17.57.241");//qq smtp 服务器
+	// 连接邮件服务器，如果连接后没有响应，则2 秒后重新连接
+	sockfd = open_socket((struct sockaddr *)&their_addr);
+	memset(rbuf,0,1500);
+	while(recv(sockfd, rbuf, 1500, 0) == 0)
+	{
+		printf("reconnect...\n");
+		sleep(2);
+		//close(sockfd);
+		sockfd = open_socket((struct sockaddr *)&their_addr);
+ 
+		memset(rbuf,0,1500);
+	}
+ 
+	//*printf("%s\n", rbuf);
+ 
+	// EHLO
+	memset(buf, 0, 1500);
+	sprintf(buf, "EHLO abcdefg-PC\r\n");
+	send(sockfd, buf, strlen(buf), 0);
+	memset(rbuf, 0, 1500);
+	recv(sockfd, rbuf, 1500, 0);
+	//*printf("%s\n", rbuf);
+ 
+	// AUTH LOGIN
+	memset(buf, 0, 1500);
+	sprintf(buf, "AUTH LOGIN\r\n");
+	send(sockfd, buf, strlen(buf), 0);
+	//*printf("%s\n", buf);
+	memset(rbuf, 0, 1500);
+	recv(sockfd, rbuf, 1500, 0);
+	//*printf("%s\n", rbuf);
+ 
+	// USER
+	memset(buf, 0, 1500);
+	sprintf(buf,"851056505");//你的qq号
+	memset(login, 0, 128);
+	base64(login, buf, strlen(buf));
+	sprintf(buf, "%s\r\n", login);
+	send(sockfd, buf, strlen(buf), 0);
+	//*printf("%s\n", buf);
+	memset(rbuf, 0, 1500);
+	recv(sockfd, rbuf, 1500, 0);
+	//*printf("%s\n", rbuf);
+ 
+	// PASSWORD
+	sprintf(buf, "wmxgpxgmqdhybbad");//你的qq密码
+	memset(pass, 0, 128);
+	base64(pass, buf, strlen(buf));
+	sprintf(buf, "%s\r\n", pass);
+	send(sockfd, buf, strlen(buf), 0);
+	//*printf("%s\n", buf);
+ 
+	memset(rbuf, 0, 1500);
+	recv(sockfd, rbuf, 1500, 0);
+	//*printf("%s\n", rbuf);
+ 
+	// MAIL FROM
+	memset(buf, 0, 1500);
+	sprintf(buf, "MAIL FROM: <851056505@qq.com>\r\n");
+	send(sockfd, buf, strlen(buf), 0);
+	memset(rbuf, 0, 1500);
+	recv(sockfd, rbuf, 1500, 0);
+	//*printf("%s\n", rbuf);
+ 
+	// RCPT TO 第一个收件人
+	sprintf(buf, "RCPT TO:<%s>\r\n", email);
+	send(sockfd, buf, strlen(buf), 0);
+	memset(rbuf, 0, 1500);
+	recv(sockfd, rbuf, 1500, 0);
+	//*printf("%s\n", rbuf);
+ 
+	// DATA 准备开始发送邮件内容
+	sprintf(buf, "DATA\r\n");
+	send(sockfd, buf, strlen(buf), 0);
+	memset(rbuf, 0, 1500);
+	recv(sockfd, rbuf, 1500, 0);
+	//*printf("%s\n", rbuf);
+ 
+	// 发送邮件内容，\r\n.\r\n内容结束标记
+	sprintf(buf, "%s\r\n.\r\n", body);
+	send(sockfd, buf, strlen(buf), 0);
+	memset(rbuf, 0, 1500);
+	recv(sockfd, rbuf, 1500, 0);
+	//*printf("%s\n", rbuf);
+ 
+	// QUIT
+	sprintf(buf, "QUIT\r\n");
+	send(sockfd, buf, strlen(buf), 0);
+	memset(rbuf, 0, 1500);
+	recv(sockfd, rbuf, 1500, 0);
+	//*printf("%s\n", rbuf);
+ 
+// VC2005 需要使用
+	//closesocket(sockfd);
+	close(sockfd);
+	#ifdef WIN32
+	WSACleanup();
+	#endif
+	return;
+ 
+}
+	// 打开TCP Socket连接
+int open_socket(struct sockaddr *addr)
+{
+		int sockfd = 0;
+		sockfd=socket(PF_INET, SOCK_STREAM, 0);
+		if(sockfd < 0)
+		{
+			fprintf(stderr, "Open sockfd(TCP) error!\n");
+			exit(-1);
+		}
+		if(connect(sockfd, addr, sizeof(struct sockaddr)) < 0)
+		{
+			fprintf(stderr, "Connect sockfd(TCP) error!\n");
+			exit(-1);
+		}
+		return sockfd;
+} 
 
